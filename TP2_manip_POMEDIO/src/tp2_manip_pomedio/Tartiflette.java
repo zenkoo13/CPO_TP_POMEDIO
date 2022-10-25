784f0e9c1643efcd47ed8f66ca9d02f71649d81d
kenzopomedio <kenzopomedio@10.3.60.199>
2022-10-25 11:54:01 +0200

    tp2manip q1 à 4

---
+++ TP2_manip_POMEDIO/src/tp2_manip_pomedio/Tartiflette.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_pomedio;
+
+/**
+ *
+ * @author kenzopomedio
+ */
+public class Tartiflette {
+    int nbCalories;
+    
+    public Tartiflette(int nbCalo){
+        nbCalories=nbCalo;
+    }
+    
+}
