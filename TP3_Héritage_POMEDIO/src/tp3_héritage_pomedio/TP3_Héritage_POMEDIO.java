f093d0b4c4d742fe6e8df50e6ff3a1c7991b907f
kenzopomedio <kenzopomedio@10.3.60.199>
2022-11-08 15:02:11 +0100

    tp3 heritage création du projet

---
+++ TP3_Héritage_POMEDIO/src/tp3_héritage_pomedio/TP3_Héritage_POMEDIO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_héritage_pomedio;
+
+/**
+ *
+ * @author kenzopomedio
+ */
+public class TP3_Héritage_POMEDIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
